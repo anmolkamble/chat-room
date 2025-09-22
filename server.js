265c41837339974ed52e3ef5d516f6e1f25d79ac
anmolkamble <76765145+anmolkamble@users.noreply.github.com>
2025-09-22 23:07:22 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var server = http.createServer(function(req, res) {});
+var io = require('socket.io')(server);
+io.on('connection', function(socket) {
+ console.log('a user connected');
+ socket.on('disconnect', function() {
+   console.log('user disconnected');
+ });
+ socket.on('chat message', function(msg) {
+   console.log('message: ' + msg);
+   io.emit('chat message', msg);
+ });
+});
+server.listen(3000, function() {
+ console.log('server up and running');
+});
